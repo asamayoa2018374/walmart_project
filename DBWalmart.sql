9686bd9ec27771f79698ac2068938ddd738c7581
JSolares2016515 <jsolares-2016515@kinal.edu.gt>
2019-09-03 17:22:38 -0600

    Modified Database for design

--- DBWalmart.sql
+++ DBWalmart.sql
@@ -5,10 +5,10 @@ use DBWalmart;
 create table Productos(
 	codigo int not null auto_increment,
     nombre varchar(25) not null,
+    descripcion varchar(50) not null,
+    categoria varchar(20) not null,
+    cantidad int not null,
     precioUnitario decimal(6,2) not null,
     precioDocena decimal(6,2) not null,
-    categoria varchar(20) not null,
-    descripcion varchar(50) not null,
-    cantidad int not null,
     primary key PK_Codigo (codigo)
 );
