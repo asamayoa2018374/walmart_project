b2de0362b6b6d8f8a253c5a10d160b48b22f1230
asamayoa2018374 <asamayoa-2018374@kinal.edu.gt>
2019-09-03 16:47:01 -0600

    Initial configuration

---
+++ DBWalmart.sql
@@ -0,0 +1,14 @@
+create database DBWalmart;
+
+use DBWalmart;
+
+create table Productos(
+	codigo int not null auto_increment,
+    nombre varchar(25) not null,
+    precioUnitario decimal(6,2) not null,
+    precioDocena decimal(6,2) not null,
+    categoria varchar(20) not null,
+    descripcion varchar(50) not null,
+    cantidad int not null,
+    primary key PK_Codigo (codigo)
+);
---
+++ ProyectoWalmart/src/main/java/com/walmart/system/Main.java
@@ -0,0 +1,7 @@
+package com.walmart.system;
+
+public class Main {
+    public static void main(String[]args){
+        
+    }
+}
