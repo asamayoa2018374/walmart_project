af1402f5949925fe5225b149db23bad3119f7a05
JSolares2016515 <jsolares-2016515@kinal.edu.gt>
2019-09-04 13:36:45 -0600

    Added one line for auto increment ID's

--- ProyectoWalmart/src/main/java/com/walmart/domain/Producto.java
+++ ProyectoWalmart/src/main/java/com/walmart/domain/Producto.java
@@ -4,6 +4,8 @@ package com.walmart.domain;
 import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import lombok.Data;
@@ -13,6 +15,7 @@ import lombok.Data;
 @Data
 public class Producto implements Serializable{
     @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     @Column
     private int codigo;
     @Column
